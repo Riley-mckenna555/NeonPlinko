b38e41c82edc4682d52b007be80f4dfb1918fd8b
Riley-mckenna555 <000975214@mohawkcollege.ca>
2025-12-05 11:03:05 -0500

    add base project

---
+++ plinko-unity/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1;
+    public GameObject disc;
+    public CameraFollow cameraFollow;
+    private GameObject activeDisc;
+
+    void Update()
+    {
+        Move();
+        DropDisc();
+    }
+
+    void DropDisc()
+    {
+        // 
+        if (Input.GetButtonDown("Fire1") && activeDisc == null)
+        {
+            Vector3 position = transform.position;
+            Quaternion rotation = transform.rotation;
+            activeDisc = Instantiate(disc, position, rotation);
+            cameraFollow.FollowDisc(activeDisc);
+        }
+    }
+
+    void Move()
+    {
+        // 
+        float movementX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        Vector3 offset = new Vector3(movementX, 0, 0);
+        transform.position += offset;
+    }
+}
